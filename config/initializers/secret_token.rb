cca5b7d9df5e349ef925c4d0d6ba270a04d7a2a2
Eric Toulson <ebtoulson@gmail.com>
2012-03-23 18:40:12 -0400

    Init commit for the RoR app tictactoe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tictactoe::Application.config.secret_token = '011b43d6662b16175ff35817326b8863ff603bef206ff3240fb2e6b0d15b2c1a21964a462e44d0348ce8e55bd7ede790daa080102eb98c2e1127f9e907c25dc4'
