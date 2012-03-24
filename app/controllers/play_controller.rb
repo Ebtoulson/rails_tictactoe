748d9e447b2187e476eb03d8727909f2ab07831e
Eric Toulson <ebtoulson@gmail.com>
2012-03-24 17:39:05 -0400

    removed excess blueprint files, made routes more RESTful, finished ajax loading of the tictactoe board

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,5 +1,29 @@
 class PlayController < ApplicationController
-	def play
-		@board = TicTacToe.new
+	respond_to :js
+
+	def show
+		@board = get_board
 	end
+
+	def edit #getting the move and returning the computers move
+		x,y = params[:numbers].keys[0].split("")
+		@board = get_board.set_move("x",x,y)
+		respond_to do |format|
+			format.js
+		end
+	end
+
+	def new
+		session[:board] = nil
+		redirect_to :action => 'show'
+	end
+
+	private
+
+		def get_board
+			if session[:board] == nil
+				session[:board] = TicTacToe.new
+			end
+			return session[:board]
+		end
 end
--- app/models/tic_tac_toe.rb
+++ app/models/tic_tac_toe.rb
@@ -1,11 +1,20 @@
 class TicTacToe < ActiveRecord::Base
-	attr_reader :board
+	attr_accessor :board
 
 	def initialize
 		@board = [
-			["", "", ""],
-			["", "", ""],
-			["", "", ""]
+			['','',''],
+			['','',''],
+			['','','']
 		]
+	end
+
+	def at(x,y)
+		return @board[x][y]
+	end
+
+	def set_move(character, x, y)
+		@board[x.to_i][y.to_i] = character.to_s
+		return @board
 	end
 end
